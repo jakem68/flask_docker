3e9238bd78954b4030e83d51fb0f06bceef20eff
jan <jan.kempeneers2@gmail.com>
2019-12-06 22:37:06 +0100

    yaml_load_dump

---
+++ First_app/app/yaml_load_dump.py
@@ -0,0 +1,25 @@
+__author__ = 'Jan Kempeneers'
+
+import sys
+import yaml
+
+def yaml_load(yaml_file):
+    with open(yaml_file) as file:
+        yaml_dict = yaml.full_load(file)
+    print(yaml_dict["mqtt_server"]["url"])
+    return yaml_dict
+
+def yaml_dump(yaml_dict, file):
+    with open(file, 'w') as file:
+        documents = yaml.dump(yaml_dict, file)
+
+def run(yaml_file):
+    yaml_dict = yaml_load(yaml_file)
+    print(yaml_dict)
+
+def main():
+    yaml_file = sys.argv[1]
+    run(yaml_file)
+
+if __name__ == '__main__':
+    main()
