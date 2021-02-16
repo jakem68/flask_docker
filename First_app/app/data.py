654891c3cb9f0c7106812fd1c5abf6e5429580f5
jan <jan.kempeneers2@gmail.com>
2021-02-16 20:19:48 +0100

    updated 2021

--- First_app/app/data.py
+++ First_app/app/data.py
@@ -3,14 +3,22 @@
 __author__ = 'Jan Kempeneers'
 
 # import paho.mqtt.client as mqtt
-import os, socket, math, json
+import os, socket, math, json, time
 from subprocess import call, Popen, PIPE
+from yaml_load_dump import yaml_load, yaml_dump
 
 def get_sine(x_value):
     sine_amplitude=50
     sine_displacement=50
     sine_value = int(round(math.sin(math.radians(x_value))*sine_amplitude))+sine_displacement
     return sine_value
+
+def update_sine_dataset(sine_value, sine_dataset):
+    sine_dataset["timestamps"].append(int(time.time()))
+    sine_dataset["timestamps"].pop(0)
+    sine_dataset["sine_values"].append(sine_value)
+    sine_dataset["sine_values"].pop(0)
+    return sine_dataset
 
 def get_ip():
     ip_address = ''
@@ -26,4 +34,3 @@ def get_ip():
     except:
         ip_address = ': Sorry, no network available, so no IP address to show.'
     return ip_address
-
